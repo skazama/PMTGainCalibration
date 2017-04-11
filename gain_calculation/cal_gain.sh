b1147c5f4d70971a1c05e0fd40af1b003de64c18
Shingo Kazama <kazama@physik.uzh.ch>
2017-04-11 10:40:27 +0200

    Update cal_gain.sh

--- gain_calculation/cal_gain.sh
+++ gain_calculation/cal_gain.sh
@@ -16,7 +16,7 @@ do
         ./cal_gain $bg_file ${mode3} rootfile/$output3 2 $bg_name
 
         hadd -f rootfile/${run}.root rootfile/$output3 rootfile/$output2 rootfile/$output1
-	rm rootfile/$output1 rootfile/$output2 rootfile/$output3
+	# rm rootfile/$output1 rootfile/$output2 rootfile/$output3
 
 	gain_file=`ls txt/gain*_2_*${bg_name}.txt | sed -e "s/_2_//" | sed -e "s/${bg_name}//"`
 	cat txt/gain*_2_*${bg_name}.txt txt/gain*_1_*${bg_name}.txt txt/gain*_0_*${bg_name}.txt > ${gain_file}
